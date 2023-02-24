e6d0c3d15e3e1d8f67e708f080bd7d43c442dfa2
Maciej Myszka <maciejmyszka99@gmail.com>
2023-02-24 18:40:10 +0100

    Added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import './App.css';
 import { MainView } from './views/Main';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme';
 
 function App() {
-  return <MainView />;
+  return (
+    <ChakraProvider theme={theme}>
+      <MainView />
+    </ChakraProvider>
+  );
 }
 
 export default App;
